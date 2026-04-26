08c80da6b98ddad29abc92b8765f583b98081874
Ubuntu <ubuntu@ip-172-31-79-245.ec2.internal>
2026-04-26 18:09:19 +0000

    thierd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 function requestHandler(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from dixit server!");
+  res.end("Hello from deep  server!");
 }
 
 const server = http.createServer(requestHandler);
