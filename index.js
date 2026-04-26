97e86337e8fe24448233421a48e0d1b216e8b53f
Ubuntu <ubuntu@ip-172-31-79-245.ec2.internal>
2026-04-26 19:20:21 +0000

    msjjsk

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 function requestHandler(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from never server!");
+  res.end("Hello rcb!");
 }
 
 const server = http.createServer(requestHandler);
