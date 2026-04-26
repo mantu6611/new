856e5466ef184b2343b48b676bca2c1ae4f7507a
Ubuntu <ubuntu@ip-172-31-79-245.ec2.internal>
2026-04-26 19:41:35 +0000

    mycdc

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 function requestHandler(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("never give up");
+  res.end("never give up maksood");
 }
 
 const server = http.createServer(requestHandler);
